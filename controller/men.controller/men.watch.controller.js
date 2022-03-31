c0766545141b584f3a419ba91a4510e45f5630e3
kirankumar3117 <ch.kirankumar311@gmail.com>
2022-04-01 01:39:52 +0530

    added_mens_watch_controller

---
+++ controller/men.controller/men.watch.controller.js
@@ -0,0 +1,25 @@
+const express=require("express");
+
+const router=express.Router();
+
+const Menwatch=require("../../model/men.model/men.watch.model")
+
+
+router.post("/men/watch",(req,res)=>{
+    try{
+    const user= new Menwatch({
+        name:req.body.name,
+        image:req.body.image,
+        mrp:req.body.mrp,
+        discount:req.body.discount,
+        rs:req.body.rs,
+        star:req.body.star
+    });
+
+    res.status(200).send(user)
+}catch(err){
+    res.status(400).send("somthing is missing in post body" , {message:err.message})
+}
+
+
+})
---
+++ model/men.model/men.watch.model.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+
+const menswatchesSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    Image:{type:String,required:true},
+    mrp:{type:Number,required:true},
+    discount:{type:Number,required:true},
+    rs:{type:Number,required:true},
+    star:{type:Number,required:true},
+
+});
+
+const Menswatches= mongoose.model("menswatch",menswatchesSchema);
+
+module.exports=Menswatches;
+
